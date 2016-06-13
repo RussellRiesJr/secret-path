b8edd42810bffea01b7698635d89be27a0a48832
Russell Ries <riesru@gmail.com>
2016-06-13 13:29:29 -0500

    add firebase factory

---
+++ content/main.js
@@ -0,0 +1,9 @@
+angular.module('secret', ['ngRoute']);
+
+firebase.initializeApp({
+    apiKey: "AIzaSyALb1dIBcyikJwzhS8u8kDKb4mAezdwNok",
+    authDomain: "the-secret-path.firebaseapp.com",
+    databaseURL: "https://the-secret-path.firebaseio.com",
+    storageBucket: "the-secret-path.appspot.com",
+  });
+
