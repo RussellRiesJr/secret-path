85275c0ee221d65b0bddf72529f4830fe38a4008
Russell Ries <riesru@gmail.com>
2016-06-13 14:50:54 -0500

    add config in main.js

--- content/main.js
+++ content/main.js
@@ -1,9 +1,18 @@
-angular.module('secret', ['ngRoute']);
+angular.module('secret', ['ngRoute'])
+  .config(($routeProvider) => {
+    $routeProvider
+      .when('/map', {
+        controller: 'mapCtrl',
+        controllerAs: 'map',
+        templateUrl: 'content/map.html'
+      })
+      .otherwise('/')
+  })
 
 firebase.initializeApp({
     apiKey: "AIzaSyALb1dIBcyikJwzhS8u8kDKb4mAezdwNok",
     authDomain: "the-secret-path.firebaseapp.com",
     databaseURL: "https://the-secret-path.firebaseio.com",
     storageBucket: "the-secret-path.appspot.com",
-  });
+  })
 
