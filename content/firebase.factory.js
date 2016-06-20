9c09ea658bf1a9ea631ad77e8873e05ffee01ace
Russell Ries <riesru@gmail.com>
2016-06-20 10:45:21 -0500

    add time check function

--- content/firebase.factory.js
+++ content/firebase.factory.js
@@ -2,6 +2,7 @@ angular.module('secret')
   .factory("firebaseFactory", ($timeout) => {
 
     return {
+      // Putting user route info into Firebase
       setInfo: function (secretPath) {
         return firebase.database().ref('/paths').push(secretPath).then(response => response.key)
       }
--- content/map.controller.js
+++ content/map.controller.js
@@ -33,7 +33,31 @@ angular.module('secret')
     firebase.database().ref(`/paths/${pathName}`).once("value").then(function(snapshot) {
       const startTime = snapshot.val().dateTime
       const startAddress = snapshot.val().startPoint
+      const userStartLat = snapshot.val().coords.lat
+      const userStartLng = snapshot.val().coords.lng
       let objStart = new Date(startTime);
       directions.innerHTML = `<h5>This path will be available by going to ${startAddress} at ${startTime}<h5>`;
+      map.timeCheck(objStart);
     })
+
+    // Creating emtry variable to hold Interval Id
+    let intervalId;
+
+    // Creating Interval for checking time
+    map.setTime = function (objStart) {
+      intervalId = setInterval(map.timeCheck(objStart), 300000);
+      map.timeCheck(objStart);
+    }
+
+    // Checking current time against user input access time
+    map.timeCheck = function (objStart) {
+      let now = new Date ();
+      let currentTime = now.getTime();
+      let goTime = objStart - currentTime;
+      // console.log('is it time', goTime);
+      if(goTime <= 0) {
+        clearInterval(intervalId);
+        map.checkUserPosition();
+      }
+    }
   })
